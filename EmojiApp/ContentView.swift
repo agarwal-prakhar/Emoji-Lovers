8e5767492c554f99fba21c1c0fb5e59c288aa371
Prakhar Agarwal <realprakhar@outlook.com>
2023-10-22 03:03:23 +0530

    Update ContentView.swift
    iOS Application

--- EmojiApp/ContentView.swift
+++ EmojiApp/ContentView.swift
@@ -7,15 +7,28 @@
 
 import SwiftUI
 
+enum Emoji: String,CaseIterable {
+    
+    case 🤑, 🚀, 🎉, 🎁, 🪐
+}
+
 struct ContentView: View {
+    @State var selection: Emoji = .🚀
+ 
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationView{
+            VStack{
+                Text(selection.rawValue)
+                    .font(.system(size: 150))
+                Picker("Select Emoji", selection: $selection){
+                    ForEach(Emoji.allCases,id: \.self){ emoji in Text(emoji.rawValue)
+                    }
+                }
+                
+                .pickerStyle(.palette).padding().navigationTitle("Emoji Lovers!")
+            }
         }
-        .padding()
+
     }
 }
 
